8747a3495edfa31adae62d74d717b7c0c59ea3b6
John Idoko <95774230+KhengJohn@users.noreply.github.com>
2022-09-02 23:42:02 +0100

    new port

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,14 @@ import projectdata from "./projectdata";
 
 function Projects(){ 
     const projectElements = projectdata.map(project => {
-        return <PCard Title={project.projectTitle} description={project.description} lang1={project.lang1}
-        lang2={project.lang2} lang3={project.lang3} lang4={project.lang4} livelink={project.livelink} gitlink={project.gitlink} />
+        return <PCard   Title={project.projectTitle} 
+                        description={project.description} 
+                        lang1={project.lang1}
+                        lang2={project.lang2} 
+                        lang3={project.lang3} 
+                        lang4={project.lang4} 
+                        livelink={project.livelink} 
+                        gitlink={project.gitlink} />
     })
     return(
         <section className="projects-section">
