cab47259e95a83122ad4199d8cc676affde097f7
John Idoko <idokojohn42@yahoo.com>
2023-08-22 21:30:37 +0100

    Nav and new structure

--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -3,52 +3,54 @@ import { useState, useRef } from "react";
 const Logo = `${process.env.PUBLIC_URL}/assets/images/logo192.png`;
 
 function Nav() {
-  // const hamburger = document.querySelector(".hamburger");
-  // const navLinks = document.querySelector(".nav-links");
-  // const links = document.querySelectorAll(".nav-links li");
-  //  const navLinks = useRef();
-  // const navLinksLi = useRef();
-  // const hamburger = useRef();
-  // hamburger.addEventListener('click',
-  // function hamburgerO (){
-
-  //     navLinks.classList.toggle("open");
-  //     navLinksLi.forEach(navLinksLi => {
-  //         navLinksLi.classList.toggle("fade");
-  //     });
-  //     hamburger.classList.toggle("toggle");
-  // };
+  const [toggle, setToggle] = useState(false);
 
   const handleClick = (event) => {
-    // event.currentTarget.classList.toggle('open');
-    event.currentTarget.classList.toggle("toggle");
-    // event.currentTarget.classList.toggle('fade');
+    setToggle(!toggle);
   };
 
+  const scrollToSection = (sectionId) => {
+    setToggle(!toggle);
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: "smooth" });
+  };
   return (
     <nav>
-      <div class="navii">
-        <div class="logo">
-          <img src={Logo} alt="Logo Image" class="logoTop" />
+      <div className="navii">
+        <div className="logo">
+          <img src={Logo} alt="Logo Image" className="logoTop" />
         </div>
-        <div onClick={handleClick} class="hamburger">
-          <div class="line1"></div>
-          <div class="line2"></div>
-          <div class="line3"></div>
+        <div
+          onClick={handleClick}
+          className={toggle ? `hamburger toggle ` : `hamburger`}
+        >
+          <div className="line1 "></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
         </div>
 
-        <ul class="nav-links">
+        <ul className={toggle ? `nav-links open fade` : `nav-links`}>
           <li>
-            <a href="">Home</a>
+            <a onClick={() => scrollToSection("section1")} href="#section1">
+              Home
+            </a>
           </li>
           <li>
-            <a href="">About</a>
+            <a onClick={() => scrollToSection("section2")} href="#section2">
+              About
+            </a>
           </li>
           <li>
-            <a href="">Work</a>
+            <a onClick={() => scrollToSection("section3")} href="#section3">
+              Work
+            </a>
           </li>
           <li>
-            <a class="btn-project" href="">
+            <a
+              className="btn-project"
+              target="_blank"
+              href="https://drive.google.com/file/d/1kPu3t_0rYlQxIA94fG7au_TS3fEigcox/view?usp=drivesdk"
+            >
               Resume
             </a>
           </li>
